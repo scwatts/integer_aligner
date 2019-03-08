a7948fb7f6c9c52a56883dbf6e6537d6109001e9
scwatts <s.watts2@student.unimelb.edu.au>
2019-03-08 12:50:48 +1100

    Specify scoring parameters

--- src/aligner.cpp
+++ src/aligner.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 
 
+#include <seqan/score.h>
 #include <seqan/graph_msa.h>
 #include <seqan/basic.h>
 
@@ -87,7 +88,9 @@ seqan::StringSet<IntegerString> prepare_alignment_data(const std::vector<Integer
 
 seqan::Align<IntegerString> perform_alignment(seqan::StringSet<IntegerString> &integer_set) {
     seqan::Align<IntegerString> alignment(integer_set);
-    seqan::globalMsaAlignment(alignment, seqan::EditDistanceScore());
+    // TODO: demonstrate different scoring params (seqan/score/score_simple.h#L80)
+    seqan::Score<int> scoring_scheme(5, -6, -2, -11);
+    seqan::globalMsaAlignment(alignment, scoring_scheme);
     return alignment;
 }
 
